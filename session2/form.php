0c6de61e647b7becda1379833328ba136e923065
Chad Luong <apple.luong1905@gmail.com>
2019-10-02 21:06:59 +0700

    update

--- session2/form.php
+++ session2/form.php
@@ -1,21 +1,11 @@
 <h1>Form example</h1>
 <?php 
 	if (isset($_POST['register'])) {
-		echo "Show get<br>";
-		echo $_GET['name'];
-		echo "<br>";
-		echo $_GET['email'];
-
 		//
 		echo "<br>Show post<br>";
 		echo $_POST['name'];
 		echo "<br>";
 		echo $_POST['email'];
-			//
-		echo "<br>Show request<br>";
-		echo $_REQUEST['name'];
-		echo "<br>";
-		echo $_REQUEST['email'];
 	}
 ?>
 <form name="registerForm" action="form.php" method="POST">
