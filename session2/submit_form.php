09d8042518e3ba8f3d5803a7a4d061ba7b3a6054
Chad Luong <apple.luong1905@gmail.com>
2019-10-02 21:02:55 +0700

    update

---
+++ session2/submit_form.php
@@ -0,0 +1,17 @@
+<?php 
+	echo "Show get<br>";
+	echo $_GET['name'];
+	echo "<br>";
+	echo $_GET['email'];
+
+	//
+	echo "<br>Show post<br>";
+	echo $_POST['name'];
+	echo "<br>";
+	echo $_POST['email'];
+		//
+	echo "<br>Show request<br>";
+	echo $_REQUEST['name'];
+	echo "<br>";
+	echo $_REQUEST['email'];
+?>
