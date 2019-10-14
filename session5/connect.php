107d9e40e43b32051cbb792f28f1783eb3a5be7e
Chad Luong <apple.luong1905@gmail.com>
2019-10-14 19:40:28 +0700

    update

--- session5/connect.php
+++ session5/connect.php
@@ -8,18 +8,10 @@
 
 	// Change character set to utf8
 	mysqli_set_charset($connect,"utf8");
+	
 	// Check connection
 	if (mysqli_connect_errno())
   {
   	echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
-
-  $title = "Sản phẩm chất"; // $title = $_POST['title'];
-  
-  $sql = "INSERT INTO products (title, description, image, price, created, updated) VALUES ('$title', 'mo ta 1111', 'myimage.jpg', 1240, '2019-10-09 21:00:00', '2019-10-09 21:00:00')
-";
-	if (mysqli_query($connect, $sql) === TRUE) {
-		echo "Insert success!";
-	}
-
 ?>
